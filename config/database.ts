8c4fc22aedb6c5aade6005d5ec5e089c7a9b9ffb
tytata <33824383+tytata@users.noreply.github.com>
2025-03-24 16:09:37 +0700

    update from db railway
    update from using db railway

--- config/database.ts
+++ config/database.ts
@@ -1,21 +1,21 @@
 export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: { rejectUnauthorized: false },
-    },
-    debug: false,
-  },
   // connection: {
   //   client: 'postgres',
-  //   connection: env('DATABASE_URL'),
+  //   connection: {
+  //     host: env('DATABASE_HOST'),
+  //     port: env.int('DATABASE_PORT', 5432),
+  //     database: env('DATABASE_NAME'),
+  //     user: env('DATABASE_USERNAME'),
+  //     password: env('DATABASE_PASSWORD'),
+  //     ssl: { rejectUnauthorized: false },
+  //   },
   //   debug: false,
-  // }
+  // },
+  connection: {
+    client: 'postgres',
+    connection: env('DATABASE_URL'),
+    debug: false,
+  }
 });
 
 
